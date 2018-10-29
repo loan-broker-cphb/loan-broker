43e9d42e4b02dc0e01917e72043f125807783933
Mikkel Ziemer <mzhjensen@gmail.com>
2018-10-29 16:07:35 +0100

    Added spring boot project for API part

---
+++ loan-broker-api/src/main/java/com/loanbroker/api/LoanBrokerApiApplication.java
@@ -0,0 +1,12 @@
+package com.loanbroker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanBrokerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanBrokerApiApplication.class, args);
+    }
+}
---
+++ loan-broker-api/src/main/java/com/loanbroker/api/resources/QuoteResource.java
@@ -0,0 +1,22 @@
+package com.loanbroker.api.resources;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@RestController
+@RequestMapping("quote")
+@Produces(MediaType.APPLICATION_JSON)
+public class QuoteResource {
+
+    @GetMapping
+    public String getQuote() {
+        return "Good one!";
+    }
+}
