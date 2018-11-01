deeaac6697493d8f0dc5dd38dadec10b987c0e1c
Mikkel Ziemer <mzhjensen@gmail.com>
2018-11-01 13:31:48 +0100

    Made a working example with custom object parsing and readin from rabbitMQ

--- loan-broker-aggregator/src/main/java/com/loanbroker/aggregator/Receiver.java
+++ loan-broker-aggregator/src/main/java/com/loanbroker/aggregator/Receiver.java
@@ -1,10 +1,10 @@
 package com.loanbroker.aggregator;
 
+import com.loanbroker.commons.model.NormalizerAggregator;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 @Component
 public class Receiver {
@@ -16,8 +16,9 @@ public class Receiver {
         this.rabbitTemplate = rabbitTemplate;
     }
 
-    public void receiveMessage(String message) {
-        System.out.println(message);
+    public void receiveMessage(NormalizerAggregator message) {
+        System.out.println(message.getSsn());
+        System.out.println(message.getInterestRate());
     }
 
     public CountDownLatch getLatch() {
--- loan-broker-aggregator/src/main/java/com/loanbroker/aggregator/Runner.java
+++ loan-broker-aggregator/src/main/java/com/loanbroker/aggregator/Runner.java
@@ -1,9 +1,11 @@
 package com.loanbroker.aggregator;
 
+import com.loanbroker.commons.model.NormalizerAggregator;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -20,8 +22,10 @@ public class Runner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Sending message...");
-        rabbitTemplate.convertAndSend(LoanBrokerAggregatorApplication.directExchangeName, "ping", "Hello from RabbitMQ!");
-        rabbitTemplate.convertAndSend(LoanBrokerAggregatorApplication.directExchangeName, "ping2", "Hello from RabbitMQ 2!");
+        NormalizerAggregator normalizerAggregator = new NormalizerAggregator();
+        normalizerAggregator.setSsn("12345");
+        normalizerAggregator.setInterestRate(BigDecimal.valueOf(1.12345));
+        rabbitTemplate.convertAndSend(LoanBrokerAggregatorApplication.directExchangeName, "ping", normalizerAggregator);
         receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
     }
 }
