8e696f9eb49c01b77777cee4c302658396841b61
Cherry Rose Semena <cph-cs241@cphbusiness.dk>
2018-11-17 18:03:37 +0100

    connect credit bureau soap service

---
+++ loan-broker-credit-score/src/main/java/creditbureau/CreditScore.java
@@ -0,0 +1,60 @@
+
+package creditbureau;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for creditScore complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="creditScore">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="ssn" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "creditScore", propOrder = {
+    "ssn"
+})
+public class CreditScore {
+
+    protected String ssn;
+
+    /**
+     * Gets the value of the ssn property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSsn() {
+        return ssn;
+    }
+
+    /**
+     * Sets the value of the ssn property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSsn(String value) {
+        this.ssn = value;
+    }
+
+}
