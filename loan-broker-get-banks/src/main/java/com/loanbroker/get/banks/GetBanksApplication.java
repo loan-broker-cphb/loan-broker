6d0b81de99cb291e3c23b83ad808f1f67ee1bbaf
Mikkel Ziemer <mzhjensen@gmail.com>
2018-11-19 10:57:15 +0100

    Sample application for get banks

---
+++ loan-broker-get-banks/src/main/java/com/loanbroker/get/banks/GetBanksApplication.java
@@ -0,0 +1,12 @@
+package com.loanbroker.get.banks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetBanksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetBanksApplication.class, args);
+    }
+}
