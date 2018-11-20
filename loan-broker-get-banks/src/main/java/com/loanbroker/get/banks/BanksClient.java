83da7b28533d524499916628eb3f0c52721705c2
Mikkel Ziemer <mzhjensen@gmail.com>
2018-11-20 23:45:37 +0100

    Can now start up everything in one swoop

--- loan-broker-get-banks/src/main/java/com/loanbroker/get/banks/BanksClient.java
+++ loan-broker-get-banks/src/main/java/com/loanbroker/get/banks/BanksClient.java
@@ -2,10 +2,14 @@ package com.loanbroker.get.banks;
 
 import banks.wsdl.GetBanksRequest;
 import banks.wsdl.GetBanksResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
 public class BanksClient extends WebServiceGatewaySupport {
 
+
+    @Value("${getbanks.url}")
+    private String getBanksBaseUrl;
 
     public GetBanksResponse getBanks(int creditScore, double loanAmount) {
         GetBanksRequest request = new GetBanksRequest();
@@ -13,7 +17,7 @@ public class BanksClient extends WebServiceGatewaySupport {
         request.setLoanAmount(loanAmount);
 
         GetBanksResponse response = (GetBanksResponse) getWebServiceTemplate()
-                .marshalSendAndReceive("http://localhost:8080/ws/banks", request);
+                .marshalSendAndReceive(getBanksBaseUrl + "/ws/banks", request);
 
         return response;
     }
