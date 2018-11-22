fc596361283083eaf59eb20af6543eee2de5483a
Mikkel Ziemer <mzhjensen@gmail.com>
2018-11-22 12:01:16 +0100

    Implemented translator

---
+++ loan-broker-bank-translator-rabbit/src/main/java/com/loanbroker/bank/translator/rabbit/QuoteRequestDto.java
@@ -0,0 +1,44 @@
+package com.loanbroker.bank.translator.rabbit;
+
+public class QuoteRequestDto {
+    private String ssn;
+    private int creditScore;
+    private double loanAmount;
+    private int loanDuration;
+
+    public QuoteRequestDto() {
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(int creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public double getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(double loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public int getLoanDuration() {
+        return loanDuration;
+    }
+
+    public void setLoanDuration(int loanDuration) {
+        this.loanDuration = loanDuration;
+    }
+
+}
